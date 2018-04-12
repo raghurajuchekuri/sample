231dfc09e5bd797343d902edea905688f60707da
rchekuri <rchekuri@OSI-L-0409.osius.com>
2018-04-12 14:22:49 +0530

    second commit

--- src/main/java/tests/gmail_firefox.java
+++ src/main/java/tests/gmail_firefox.java
@@ -26,8 +26,7 @@ public class gmail_firefox {
 	  public void googleopen() throws Throwable {
 		  
 		  Thread.sleep(5000);
-		  driver.get("https://google.co.in");
-		
+		  driver.get("https://google.co.in");		
 		
 	  }
 	   
